9f28b85d2d73605f318b3e83f4868a2e8de1423a
Theodore Morley <ted@elmgatefarm.com>
2023-09-30 21:06:16 -0400

    Code for dropping books

---
+++ book.gd
@@ -0,0 +1,19 @@
+extends RigidBody2D
+@export var fall_speed = 300
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _physics_process(delta):
+	#position.y += fall_speed*delta
+	pass
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
--- main.gd
+++ main.gd
@@ -1,12 +1,14 @@
 extends Node2D
 
 @export var platform_scene: PackedScene
+@export var artifact_scene: PackedScene
 var score
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$PlatformTimer.start()
+	$ArtifactTimer.start()
 	new_game()
 	pass # Replace with function body.
 
@@ -45,3 +47,18 @@ func _on_platform_timer_timeout():
 	
 	
 	
+
+
+func _on_artifact_timer_timeout():
+	#create artifacts
+	var newArtifact = artifact_scene.instantiate()
+	
+	#artifact path
+	var artifact_spawn_loc = get_node("ArtifactSpawnPath/ArtifactSpawnLocation")
+	artifact_spawn_loc.progress_ratio = randf()
+	
+	#Set random position using the created var
+	newArtifact.position = artifact_spawn_loc.position
+	
+	#spawn the platform
+	add_child(newArtifact)
